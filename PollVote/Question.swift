f6c757ad1c3930dd0e85097aa008b4052d59cdc4
Aman Bhansali <Aman@Amans-MBP.fios-router.home>
2016-09-17 13:48:21 -0400

    Added Question and User objects
    Added Question and User objects

--- PollVote/Question.swift
+++ PollVote/Question.swift
@@ -1,13 +1,41 @@
+
 //
 //  Question.swift
-//  PollVote
+//  PollVoteApp
 //
-//  Created by Naman Bhansali on 9/17/16.
+//  Created by Aman Bhansali on 9/17/16.
 //  Copyright © 2016 Naman Bhansali. All rights reserved.
 //
 
 import UIKit
+import Foundation
 
-class Question: NSObject {
-
+class Questions: NSObject {
+    
+    var Question : String
+    var CreatedTime : NSDate?
+    var SubmittedBy: Int!
+    //var userPhoto: UIImage?
+    //var AnswerRespose: String
+    //var AnswerOne: Int
+    //var AnswerTwo: Int
+    //var AnswerThree: Int
+    //var AnswerFour: Int
+    
+    
+    /*init(Question: String, CreatedTime: NSDate?, SubmittedBy: Int!, userPhoto: UIImage?, AnswerResponse: String, AnswerOne: Int, AnswerTwo: Int, AnswerThree: Int, AnswerFour: Int) {
+     }*/
+    init?(Question: String, CreatedTime: NSDate?, SubmittedBy: Int!) {
+        self.Question = Question
+        self.CreatedTime = CreatedTime
+        self.SubmittedBy = SubmittedBy
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if Question.isEmpty {
+            return nil
+        }
+    }
+    
+    
+    
 }
--- PollVote/User.swift
+++ PollVote/User.swift
@@ -1,13 +1,35 @@
+
 //
 //  User.swift
-//  PollVote
+//  PollVoteApp
 //
-//  Created by Naman Bhansali on 9/17/16.
+//  Created by Aman Bhansali on 9/17/16.
 //  Copyright © 2016 Naman Bhansali. All rights reserved.
 //
 
 import UIKit
+import Foundation
 
 class User: NSObject {
-
+    
+    var Name : String
+    var Email : String
+    var Phone : String
+    
+    
+    /*init(Question: String, CreatedTime: NSDate?, SubmittedBy: Int!, userPhoto: UIImage?, AnswerResponse: String, AnswerOne: Int, AnswerTwo: Int, AnswerThree: Int, AnswerFour: Int) {
+     }*/
+    init?(Question: String, CreatedTime: NSDate?, SubmittedBy: Int!) {
+        self.Name = Name
+        self.Email = Email
+        self.Phone = Phone
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if Question.isEmpty {
+            return nil
+        }
+    }
+    
+    
+    
 }
