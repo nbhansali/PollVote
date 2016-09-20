8799d51bf21ecb8965f33f5b4ee4ae60cd72251c
Naman Bhansali <naman12345@gmail.com>
2016-09-20 00:35:28 -0400

    Created login view controller

---
+++ PollVote/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  PollVote
+//
+//  Created by Naman Bhansali on 9/20/16.
+//  Copyright © 2016 Naman Bhansali. All rights reserved.
+//
+
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
